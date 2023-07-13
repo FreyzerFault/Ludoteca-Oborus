8d10064869467eb2360a07e218b58642e994ab6e
FreyzerFault <martinezalcantara29@gmail.com>
2023-07-13 23:30:25 +0200

    fix no image placeholder

--- src/services/localData.js
+++ src/services/localData.js
@@ -2,5 +2,5 @@ export const URL_LOCAL = 'http://localhost:5173/'
 
 export const MOCK_DATA_URL = URL_LOCAL + 'mock/'
 
-export const IMAGES_URL = URL_LOCAL + 'images/'
+export const IMAGES_URL = 'images/'
 export const IMG_NOT_FOUND_URL = IMAGES_URL + 'empty box.png'
