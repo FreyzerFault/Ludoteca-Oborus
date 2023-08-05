576a07138fe55acdc9fe4a4817872afcd9e756ef
FreyzerFault <martinezalcantara29@gmail.com>
2023-08-05 02:43:16 +0200

    ya se puede ordenar en ascendente y descendente

--- src/components/BoardGameCollection.jsx
+++ src/components/BoardGameCollection.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import 'boxicons'
 import { useEffect, useState } from 'react'
 
 import { SortType, SortOrder, SortGamesBy } from '../services/bgg/bgg'
@@ -16,8 +17,15 @@ export function BoardGameCollection({ username = 'oborus', collection = [] }) {
 
   const handleSort = (e) => {
     setSortType(SortType[e.target.value])
-    console.log({ value: e.target.value })
-    e.target.checked = true
+    // e.target.checked = true
+  }
+
+  const handleOrder = (e) => {
+    setSortOrder(
+      sortOrder === SortOrder.Descending
+        ? SortOrder.Ascending
+        : SortOrder.Descending
+    )
   }
 
   return (
@@ -26,7 +34,12 @@ export function BoardGameCollection({ username = 'oborus', collection = [] }) {
         <h1 className='collection-title'>
           {collection.length} Juegos de {username}
         </h1>
-        <RadioSortButtons setSortFunc={setSortType} sortChecked={sortType} />
+        <SelectSort
+          handleSort={handleSort}
+          handleOrder={handleOrder}
+          sortChecked={sortType}
+          sortOrder={sortOrder}
+        />
       </section>
       <DataList
         className='search-results'
@@ -42,30 +55,57 @@ BoardGameCollection.propTypes = {
   collection: PropTypes.array,
 }
 
-function RadioSortButtons({ setSortFunc, sortChecked }) {
+function SelectSort({ handleSort, handleOrder, sortChecked, sortOrder }) {
   return (
-    <section className='sort-buttons'>
-      {Object.keys(SortType).map((sortKey) => (
-        <div
-          className='sort-btn'
-          key={SortType[sortKey].name}
-          onClick={() => setSortFunc(SortType[sortKey])}
-        >
-          <input
-            type='radio'
-            name='sort'
+    <section className='sort'>
+      <button onClick={handleOrder}>
+        <box-icon
+          name={sortOrder == SortOrder.Ascending ? 'sort-down' : 'sort-up'}
+          color='white'
+          size='35px'
+        ></box-icon>
+      </button>
+      <select
+        name='sort'
+        className='sort-select'
+        defaultValue={sortChecked}
+        onChange={handleSort}
+      >
+        {Object.keys(SortType).map((sortKey) => (
+          <option
+            className='sort-option'
             value={sortKey}
-            onChange={setSortFunc}
-            checked={SortType[sortKey].name === sortChecked.name}
-          />
-          <label>{SortType[sortKey].name}</label>
-        </div>
-      ))}
+            key={SortType[sortKey].name}
+          >
+            {SortType[sortKey].name}
+          </option>
+        ))}
+      </select>
     </section>
+    //   <section className='sort-buttons'>
+    //   {Object.keys(SortType).map((sortKey) => (
+    //     <div
+    //       className='sort-btn'
+    //       key={SortType[sortKey].name}
+    //       onClick={() => setSortFunc(SortType[sortKey])}
+    //     >
+    //       <input
+    //         type='radio'
+    //         name='sort'
+    //         value={sortKey}
+    //         onChange={setSortFunc}
+    //         checked={SortType[sortKey].name === sortChecked.name}
+    //       />
+    //       <label>{SortType[sortKey].name}</label>
+    //     </div>
+    //   ))}
+    // </section>
   )
 }
 
-RadioSortButtons.propTypes = {
-  setSortFunc: PropTypes.func,
+SelectSort.propTypes = {
+  handleSort: PropTypes.func,
+  handleOrder: PropTypes.func,
   sortChecked: PropTypes.any,
+  sortOrder: PropTypes.number,
 }
